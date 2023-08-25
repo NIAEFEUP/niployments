423bc94d0075a7fbe0f97f180ed88f7814990df2
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-08-25 21:50:11 +0100

    Add ansible config, and generate ansible inv from vagrant

---
+++ dev/generate-ansible-inventory.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+PARSED_HOSTS="$(vagrant ssh-config | awk '
+    BEGIN { host_section = 0 }
+    
+    # Function to print Ansible inventory entry
+    function print_inventory(host, hostname, username, port, identity_file) {
+        print host " ansible_ssh_host=" hostname " ansible_ssh_user=" username " ansible_ssh_port=" port "  ansible_ssh_private_key_file=" identity_file
+        print ""
+    }
+    
+    # Detect "Host" section
+    /^Host[[:space:]]+/ {
+        if (host_section) {
+            print_inventory(host, hostname, username, port)
+        }
+        host_section = 1
+        host = $2
+    }
+    
+    # Extract values within sections
+    host_section && /^[[:space:]]*HostName[[:space:]]+/ {
+        hostname = $2
+    }
+    host_section && /^[[:space:]]*User[[:space:]]+/ {
+        username = $2
+    }
+    host_section && /^[[:space:]]*Port[[:space:]]+/ {
+        port = $2
+    }
+    
+    host_section && /^[[:space:]]*IdentityFile[[:space:]]+/ {
+        identity_file = $2
+    }
+    # End of section
+    /^[[:space:]]*$/ {
+        if (host_section) {
+            print_inventory(host, hostname, username, port, identity_file)
+            host_section = 0
+            host = ""
+            hostname = ""
+            username = ""
+            port = ""
+        }
+    }
+    
+    END {
+        if (host_section) {
+            print_inventory(host, hostname, username, port, identity_file)
+        }
+    }
+')"
+
+router=$(echo "$PARSED_HOSTS" | grep "router")
+
+IFS=$'\n' #make ifs new line to array correctly
+nodes=(`echo "$PARSED_HOSTS" | grep "cluster"`)
+IFS=$' '
+
+INVENTORY="[routers]\n$router\n[controlplane]\n"
+
+i=0
+for node in "${nodes[@]}"
+do
+    if [ $i -eq 3 ]
+    then
+    INVENTORY+="\n[workers]\n"
+    fi
+    INVENTORY+="$node\n"
+    ((i++))
+done
+if [ $i -lt 4 ]
+then
+INVENTORY+="\n[workers]\n"
+fi
+INVENTORY+="\n[nodes:children]\ncontrolplane\nworkers
+"
+
+echo -e $INVENTORY > "ansible-inventory-dev.ini"
