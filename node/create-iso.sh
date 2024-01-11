8d5cdd7754f76e917228bea39496763c0c601943
Process-ing <rbruno.p.o@gmail.com>
2024-01-11 18:56:07 +0000

    Make Rocky linux install automatic
    Co-authored-by: Luís Duarte <luis.duarte.2003.13@hotmail.com>
    Co-authored-by: Marco Vilas Boas <marco-vb@users.noreply.github.com>

---
+++ node/create-iso.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+curl https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.3-x86_64-minimal.iso -C - -o rocky.iso
+
+mkdir -p rocky-iso
+cd rocky-iso
+bsdtar xf ../rocky.iso
+cd ..
+cp ks.cfg rocky-iso/isolinux/
+cp ks.cfg rocky-iso
+cp grub.cfg rocky-iso/EFI/BOOT/grub.cfg
+cp ninux-splash.png rocky-iso/splash.png
+cp isolinux.cfg rocky-iso/isolinux/
+cd rocky-iso
+
+mkisofs \
+    -o ../ninux.iso \
+    -b isolinux.bin \
+    -c boot.cat \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    -V "Rocky-NInux-9" -R -J -v -T isolinux/. .
+
+mkisofs \
+   -o ../ninux-efi.iso \
+   -R -J -v -d -N \
+   -x ../ninux-efi.iso \
+   -hide-rr-moved \
+   -no-emul-boot \
+   -eltorito-platform efi \
+   -eltorito-boot images/efiboot.img \
+   -V "Rocky-NInux-9" \
+   .
