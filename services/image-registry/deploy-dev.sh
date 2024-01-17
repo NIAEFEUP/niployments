0196db16dd7cf0014c19d21566f36ad93990ffe4
José Costa <sirze@live.com.pt>
2024-01-17 16:35:02 +0000

    Remove image registry service README and add Kind cluster config

--- services/image-registry/deploy-dev.sh
+++ services/image-registry/deploy-dev.sh
@@ -6,7 +6,7 @@ helm repo update
 kubectl apply -f $(dirname $0)/00-harbor-namespace.yaml
 helm upgrade --install harbor harbor/harbor \
   --namespace image-registry \
-  --values $(dirname $0)/harbor-local-values.yaml
+  --values $(dirname $0)/harbor-dev-values.yaml
 
 
-kubectl apply -f $(dirname $0)/keel-local-deployment.yaml
+kubectl apply -f $(dirname $0)/keel-dev-deployment.yaml
