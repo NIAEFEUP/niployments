2b61675d3dcf3c8e6c29843b7175536ea91ab918
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2024-01-24 18:14:39 +0000

    Modify create-iso logic by only native xorriso

--- node/create-iso.sh
+++ node/create-iso.sh
@@ -8,39 +8,20 @@ then
     exit 1
 fi
 
-if [ ! -f /usr/lib/syslinux/bios/isohdpfx.bin ];
-then
-    echo "In order to run this script you need to have the 'syslinux' binary files on your machine."
-    exit 1
-fi
-
 curl https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.3-x86_64-minimal.iso -C - -o rocky.iso
 
-mkdir -p rocky-iso
-(
-    cd rocky-iso
-    bsdtar xf ../rocky.iso
-    cd ..
-    cp ks.cfg rocky-iso/
-    cp ks.cfg rocky-iso/ks-efi.cfg
-    cp grub.cfg rocky-iso/EFI/BOOT/grub.cfg
-    cp ninux-splash.png rocky-iso/isolinux/splash.png
-    cp isolinux.cfg rocky-iso/isolinux/
-    cd rocky-iso
+rm -rf ninux.iso
 
-    xorriso -as mkisofs \
-        -isohybrid-mbr /usr/lib/syslinux/bios/isohdpfx.bin \
-     	-o ../ninux.iso \
-        -b isolinux/isolinux.bin \
-        -J -joliet-long \
-        -c boot.cat \
-        -no-emul-boot -boot-load-size 4 -boot-info-table \
-        -eltorito-alt-boot \
-        -eltorito-platform efi \
-        -e images/efiboot.img \
-        -no-emul-boot \
-	    -isohybrid-gpt-basdat \
-        -V "Rocky-NInux-9" -R -v .
-)
-
-rm -rf rocky-iso
+xorriso \
+    -indev rocky.iso \
+    -outdev ninux.iso \
+    -boot_image any replay \
+    -joliet on \
+    -system_id LINUX \
+    -compliance joliet_long_names \
+    -volid Rocky-NInux-9 \
+    -map ks.cfg ks.cfg \
+    -map ks.cfg ks-efi.cfg \
+    -map grub.cfg  EFI/BOOT/grub.cfg \
+    -map ninux-splash.png isolinux/splash.png \
+    -map isolinux.cfg isolinux/isolinux.cfg
