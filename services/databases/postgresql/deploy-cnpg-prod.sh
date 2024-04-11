de6e870c8831fa4cd594ee3a9415ada7345716c1
Process-ing <rbruno.p.o@gmail.com>
2024-04-11 10:03:57 +0100

    Create production deploy script

---
+++ services/databases/postgresql/deploy-cnpg-prod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# IMPORTANT: Deploy the development cluster first!
+# Run from the root of the repository
+
+cnpg_dir='./services/databases/postgresql'
+pods=$(cat $cnpg_dir/cnpg-cluster.yaml | awk '{if ($1 == "instances:") print $2}')
+
+kubectl apply --server-side -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.22/releases/cnpg-1.22.2.yaml
+kubectl wait --for=condition=available=true -n cnpg-system deployment/cnpg-controller-manager --timeout=120s
+
+kubectl create namespace pg
+kubectl apply -f $cnpg_dir/cnpg-cluster.yaml -n pg
+sleep 5  # Wait a little bit for first pod to be created
+init_pod_1=$(kubectl get pods -n pg | awk '{if ($1 ~ "^cnpg-cluster-1-initdb-*") print $1}')
+kubectl wait --for=delete -n pg pods/$init_pod_1 --timeout=300s
+
+for i in $(seq $pods); do
+  kubectl wait --for=condition=ready=true -n pg pods/cnpg-cluster-$i --timeout=300s
+done
