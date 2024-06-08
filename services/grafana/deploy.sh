48a97a9c8a6a882cba2f340efbe6f4af7bfb07b8
Process-ing <rbruno.p.o@gmail.com>
2024-06-08 14:09:02 +0100

    Setup Grafana

--- services/grafana/deploy.sh
+++ services/grafana/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts  # Add Grafana Helm chart
 helm repo update
 kubectl create namespace loki
-helm upgrade --install --namespace loki logging grafana/loki -f $(dirname $0)/values.yaml --set loki.auth_enabled=false --set loki.useTestSchema=true
+helm upgrade --install --namespace loki logging grafana/loki -f $(dirname $0)/values.yaml --set loki.auth_enabled=false --set loki.useTestSchema=true  # Install Loki
 # TODO: Stop using test schema
 # TODO (maybe): Use auth
+helm upgrade --install --namespace loki loki-grafana grafana/grafana  # Install Grafana
+
