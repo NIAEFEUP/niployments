e8e96e9ade532b154e4a17b5aae946ddaadb3608
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2024-06-12 14:38:19 +0100

    Change from rocky official mirrors into up's mirrors

--- node/create-iso.sh
+++ node/create-iso.sh
@@ -8,7 +8,7 @@ then
     exit 1
 fi
 
-curl https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-minimal.iso -C - -o rocky.iso
+curl http://mirrors.up.pt/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-minimal.iso -C - -o rocky.iso
 
 rm -rf ninux.iso
 
