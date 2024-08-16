250d2b23024f079400f0f90a9d5d13b717f182e1
André Lima <me@limwa.pt>
2024-08-16 23:21:20 +0100

    refactor: no need to deploy replicator

--- services/pulumi/niployments/index.ts
+++ services/pulumi/niployments/index.ts
@@ -1,6 +1,5 @@
 // ementas is an example pulumi service
 // import "./services/ementas/index.js";
-import "#resources/replicator/charts.js";
 import "./services/tts/index.js";
 
 import { CommitSignal } from "./utils/pending.js";
