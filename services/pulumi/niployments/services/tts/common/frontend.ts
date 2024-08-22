1a1489988dd7434e3003b9cf91a3a5aa24bb90d2
André Lima <me@limwa.pt>
2024-08-22 22:03:31 +0100

    feat: add keel annotations

--- services/pulumi/niployments/services/tts/common/frontend.ts
+++ services/pulumi/niployments/services/tts/common/frontend.ts
@@ -26,6 +26,10 @@ export class TTSFrontend extends pulumi.ComponentResource {
       {
         metadata: {
           namespace: args.namespace,
+          annotations: {
+            "keel.sh/policy": "force",
+            "keel.sh/match-tag": "true",
+          }
         },
         spec: {
           replicas: 1,
