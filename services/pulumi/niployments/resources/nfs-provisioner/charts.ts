12f1832b23008f729b1f6e2b4175141b03c0bc47
André Lima <me@limwa.pt>
2024-11-09 20:39:01 +0000

    fix: move assets to correct locations

--- services/pulumi/niployments/resources/nfs-provisioner/charts.ts
+++ services/pulumi/niployments/resources/nfs-provisioner/charts.ts
@@ -1,3 +1,4 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 const namespace = new k8s.core.v1.Namespace("nfs-provisioner-namespace", {
@@ -13,5 +14,5 @@ export const chart = new k8s.helm.v4.Chart("nfs-provisioner-chart", {
   repositoryOpts: {
     repo: "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner",
   },
-  valueYamlFiles: ["./values.yaml"],
+  valueYamlFiles: [new pulumi.asset.FileAsset("./assets/nfs-provisioner/values.yaml")],
 });
