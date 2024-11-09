6a5ccbdf83e5cda4a1e18a72e6d05216ca87a7ad
André Lima <me@limwa.pt>
2024-11-09 19:52:31 +0000

    fix: add namespaces to charts

--- services/pulumi/niployments/resources/keel/charts.ts
+++ services/pulumi/niployments/resources/keel/charts.ts
@@ -1,7 +1,14 @@
 import * as k8s from "@pulumi/kubernetes";
 
+const namespace = new k8s.core.v1.Namespace("keel-namespace", {
+  metadata: {
+    name: "keel",
+  },
+});
+
 export const chart = new k8s.helm.v4.Chart("keel-chart", {
   chart: "keel",
+  namespace: namespace.metadata.name,
   skipAwait: true,
   repositoryOpts: {
     repo: "https://charts.keel.sh",
--- services/pulumi/niployments/resources/nfs-provisioner/charts.ts
+++ services/pulumi/niployments/resources/nfs-provisioner/charts.ts
@@ -1,7 +1,14 @@
 import * as k8s from "@pulumi/kubernetes";
 
+const namespace = new k8s.core.v1.Namespace("nfs-provisioner-namespace", {
+  metadata: {
+    name: "nfs-provisioner",
+  },
+});
+
 export const chart = new k8s.helm.v4.Chart("nfs-provisioner-chart", {
   chart: "nfs-subdir-external-provisioner",
+  namespace: namespace.metadata.name,
   skipAwait: true,
   repositoryOpts: {
     repo: "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner",
