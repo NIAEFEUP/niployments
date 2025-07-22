552487dad93c9685fb1ee7bf13ea4f0716269a7e
Tomás Palma <up202108880@edu.fe.up.pt>
2025-07-22 19:32:44 +0100

    chore: replicate annotation in automated deployment

--- services/ni/deploy.sh
+++ services/ni/deploy.sh
@@ -2,29 +2,30 @@
 
 NI_CERT_KEY=$1
 
-
-
 NI_CERT_CERT=$2
-
-
-
 
 kubectl apply -f $(dirname $0)/website
 kubectl apply -f $(dirname $0)/nitsig
 kubectl apply -f $(dirname $0)/plausible
 
 if [[ -z "$NI_CERT_KEY" ]]; then
-    echo "NI_CERT_KEY not found... not creating certificates"
-    exit 1
+  echo "NI_CERT_KEY not found... not creating certificates"
+  exit 1
 fi
 
 if [[ -z "$NI_CERT_CERT" ]]; then
-    echo "NI_CERT_CERT not found... not creating certificates"
-    exit 1
+  echo "NI_CERT_CERT not found... not creating certificates"
+  exit 1
 fi
 
-kubectl delete secret --namespace=nitsig website-cert
 kubectl delete secret --namespace=ni-website website-cert
 
-kubectl create secret tls --namespace=nitsig website-cert --key=$NI_CERT_KEY --cert=$NI_CERT_CERT
-kubectl create secret tls --namespace=ni-website website-cert --key=$NI_CERT_KEY --cert=$NI_CERT_CERT
+kubectl create secret tls website-cert \
+  --namespace=ni-website \
+  --key=$NI_CERT_KEY \
+  --cert=$NI_CERT_CERT \
+  --dry-run=client -o yaml |
+  kubectl annotate -f - \
+    replicator.v1.mittwald.de/replicate-to=tts,nitsig |
+  kubectl apply -f -
+
