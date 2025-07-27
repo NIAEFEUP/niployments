a4a584bd7cfa253fea75f46c94e95bd2a7c2f006
Tomás Palma <up202108880@edu.fe.up.pt>
2025-07-27 15:49:55 +0100

    fix: add websiteae to index.ts pulumi

--- services/pulumi/niployments/index.ts
+++ services/pulumi/niployments/index.ts
@@ -1,6 +1,7 @@
 // ementas is an example pulumi service
 // import "./services/ementas/index.js";
 import "./services/tts/index.js";
+import "./services/websiteae/index.js";
 import "./resources/nfs-provisioner/charts.js";
 import "./resources/keel/charts.js";
 
