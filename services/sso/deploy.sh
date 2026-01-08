1cd2b9f27c8f012b7d44d7aec2c5df654af61a36
Process-ing <rbruno.p.o@gmail.com>
2026-01-08 18:15:59 +0000

    Add deploy for Hydra
    Co-authored-by: André Cerqueira <Cryptic4Life@users.noreply.github.com>

--- services/sso/deploy.sh
+++ services/sso/deploy.sh
@@ -7,4 +7,7 @@ kubectl create namespace sso
 helm repo add ory https://k8s.ory.sh/helm/charts
 helm repo update
 
-helm upgrade --install -f $ROOT/values.yaml --namespace sso kratos ory/kratos
+helm upgrade --install -f $ROOT/kratos/values.yaml --namespace sso kratos ory/kratos
+
+helm upgrade --install -f $ROOT/hydra/values.yaml --namespace sso hydra ory/hydra
+
